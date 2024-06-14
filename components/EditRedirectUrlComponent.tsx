7a208ba516906fdb9997f963ee96e0da8fa648f7
JakubUherDev <jjuherdev@gmail.com>
2024-06-15 00:24:17 +0100

    🔧 add: fix ui zoom on mobile when updating url

--- components/EditRedirectUrlComponent.tsx
+++ components/EditRedirectUrlComponent.tsx
@@ -122,7 +122,7 @@ export default function EditRedirectUrlComponent({id, callDisableEditMode, assig
                                     your new link</label>
                                 <input type="text" id="url" value={redirectUrl}
                                        onChange={e => setRedirectUrl(e.target.value)}
-                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                                       className="bg-gray-50 border border-gray-300 text-gray-900 text-[16px] rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                        placeholder="e.g. https://g.page/r/CYMJEiP2IJocEBM/review" required/>
                             </div>
                         </div>
